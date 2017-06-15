65f683ce4ade4e5ad0e6401f85f81ecc3b93beb0
Marc Storm Larsen <mslarsen1992@gmail.com>
2017-06-15 09:29:13 +0200

    Add files via upload

---
+++ goSpace/topology/message.go
@@ -0,0 +1,27 @@
+package topology
+
+// Message is the package that is send across a connection.
+// It contains the type of message, denoted by operation and either a tuple or
+// template, depending on the type of operation.
+type Message struct {
+	Operation string
+	T         interface{}
+}
+
+// CreateMessage will create the message and return it with the opertaion type
+// and tuple or template specified by the user.
+func CreateMessage(operation string, t interface{}) Message {
+	message := Message{Operation: operation, T: t}
+	return message
+}
+
+// GetOperation will return the operation of the message.
+func (message *Message) GetOperation() string {
+	return message.Operation
+}
+
+// GetBody will return the body of the message, which can be a template or a
+// tuple.
+func (message *Message) GetBody() interface{} {
+	return message.T
+}
---
+++ goSpace/topology/message_test.go
@@ -0,0 +1,52 @@
+package topology
+
+import (
+	"reflect"
+	"svn/bachelorProject/tupleSpaceFramework/constants"
+	"testing"
+)
+
+var testMessage Message
+
+func init() {
+	testMessage = CreateMessage(constants.PutRequest, []interface{}{"3", true, 4})
+}
+
+// TestMessageCreateMessage will see if the method for creating the message does
+// it as intended.
+func TestMessageCreateMessage(t *testing.T) {
+	messageManual := Message{Operation: constants.PutRequest, T: []interface{}{"3", true, 4}}
+
+	// Check equality between messages.
+	if !reflect.DeepEqual(testMessage, messageManual) {
+		t.Errorf("The method generated message %q, didn't look as expected: %q", testMessage, messageManual)
+	}
+}
+
+// TestMessageGetOperation will make sure the right part of the message is
+// returned, the operation of the message.
+func TestMessageGetOperation(t *testing.T) {
+	// Create operation string for comparison.
+	messageManualOperation := constants.PutRequest
+
+	// Get the operation of the message with method.
+	messageOperation := testMessage.GetOperation()
+
+	if !reflect.DeepEqual(messageManualOperation, messageOperation) {
+		t.Errorf("The operation from the message %q, didn't look as expected: %q", messageOperation, messageManualOperation)
+	}
+}
+
+// TestMessageGetBody will make sure the right part of the message is
+// returned, the body of the message.
+func TestMessageBodyOperation(t *testing.T) {
+	// Create body for comparison.
+	messageManualBody := []interface{}{"3", true, 4}
+
+	// Get the body of the message with method.
+	messageBody := testMessage.GetBody()
+
+	if !reflect.DeepEqual(messageManualBody, messageBody) {
+		t.Errorf("The body from the message %q, didn't look as expected: %q", messageBody, messageManualBody)
+	}
+}
