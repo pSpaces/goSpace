5010db691adba9ee1f61f6b3c9b0731875b24dad
albertolluch <albertolluch@gmail.com>
2017-08-17 11:41:28 +0200

    fridge example

--- examples/diningPhilosophers2/main.go
+++ examples/diningPhilosophers2/main.go
@@ -2,8 +2,7 @@ package main
 
 import (
 	"fmt"
-	"goSpace/goSpace/topology"
-	"goSpace/goSpace/tuplespace"
+	"goSpace/goSpace"
 	"math/rand"
 	"os"
 	"strconv"
@@ -11,14 +10,10 @@ import (
 )
 
 func main() {
-	portTable := 8081
-	portWaiter := 8082
-	table := tuplespace.CreateTupleSpace(portTable)
-	fmt.Println(table)
-	pttable := topology.CreatePointToPoint("me", "localhost", portTable)
-	waiterTS := tuplespace.CreateTupleSpace(portWaiter)
-	fmt.Println(waiterTS)
-	ptwaiter := topology.CreatePointToPoint("me", "localhost", portWaiter)
+	portTable := "8081"
+	portWaiter := "8082"
+	pttable := goSpace.NewSpace(portTable)
+	ptwaiter := goSpace.NewSpace(portWaiter)
 
 	n, err := strconv.Atoi(os.Args[1])
 	if err != nil {
@@ -43,13 +38,13 @@ func main() {
 }
 
 // placeForks places forks on the table.
-func placeForks(ptp topology.PointToPoint, n int) {
+func placeForks(table goSpace.PointToPoint, n int) {
 	for i := 0; i < n; i++ {
-		tuplespace.Put(ptp, "fork", i)
+		goSpace.Put(table, "fork", i)
 	}
 }
 
-func philosopher(pttable topology.PointToPoint, ptwaiter topology.PointToPoint, n int, fork1 int, fork2 int) {
+func philosopher(pttable goSpace.PointToPoint, ptwaiter goSpace.PointToPoint, n int, fork1 int, fork2 int) {
 	i := 0
 	for {
 		// Philosopher thinks
@@ -57,28 +52,28 @@ func philosopher(pttable topology.PointToPoint, ptwaiter topology.PointToPoint, 
 		time.Sleep(time.Duration(rand.Intn(2000)) * time.Millisecond)
 		fmt.Printf("Philosopher %d is hungry\n", n)
 		// Sends request to eat
-		tuplespace.Put(ptwaiter, "request", n)
+		goSpace.Put(ptwaiter, "request", n)
 		// LookS for permission
-		tuplespace.Query(ptwaiter, "permission", n)
+		goSpace.Query(ptwaiter, "permission", n)
 		// Grab forks
-		tuplespace.Get(pttable, "fork", fork1)
-		tuplespace.Get(pttable, "fork", fork2)
+		goSpace.Get(pttable, "fork", fork1)
+		goSpace.Get(pttable, "fork", fork2)
 		// Eats
 		fmt.Printf("Philosopher %d is eating for the %d. time\n", n, i)
 		i++
 		// Return forks
-		tuplespace.Put(pttable, "fork", fork1)
-		tuplespace.Put(pttable, "fork", fork2)
+		goSpace.Put(pttable, "fork", fork1)
+		goSpace.Put(pttable, "fork", fork2)
 		// Remove permission
-		tuplespace.Get(ptwaiter, "permission", n)
+		goSpace.Get(ptwaiter, "permission", n)
 	}
 }
 
-func waiter(ptwaiter topology.PointToPoint, n int) {
+func waiter(ptwaiter goSpace.PointToPoint, n int) {
 	for {
 		var philosopher int
 		// Get requests
-		if tuplespace.Get(ptwaiter, "request", &philosopher) {
+		if goSpace.Get(ptwaiter, "request", &philosopher) {
 
 			var neighbor1 int
 			var neighbor2 int
@@ -93,16 +88,16 @@ func waiter(ptwaiter topology.PointToPoint, n int) {
 				neighbor1 = philosopher - 1
 				neighbor2 = philosopher + 1
 			}
-			found1, conn1 := tuplespace.QueryP(ptwaiter, "permission", neighbor1)
-			found2, conn2 := tuplespace.QueryP(ptwaiter, "permission", neighbor2)
+			found1, conn1 := goSpace.QueryP(ptwaiter, "permission", neighbor1)
+			found2, conn2 := goSpace.QueryP(ptwaiter, "permission", neighbor2)
 			// See if neighbours have permission to eat
 			for found1 || found2 || !conn1 || !conn2 {
-				found1, conn1 = tuplespace.QueryP(ptwaiter, "permission", neighbor1)
-				found2, conn2 = tuplespace.QueryP(ptwaiter, "permission", neighbor2)
+				found1, conn1 = goSpace.QueryP(ptwaiter, "permission", neighbor1)
+				found2, conn2 = goSpace.QueryP(ptwaiter, "permission", neighbor2)
 			}
 			fmt.Printf("Waiter gave permission to philosopher %d\n", philosopher)
 			// Give permission to philosopher
-			tuplespace.Put(ptwaiter, "permission", philosopher)
+			goSpace.Put(ptwaiter, "permission", philosopher)
 		}
 	}
 }
