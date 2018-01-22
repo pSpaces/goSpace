ebe5840a6c9b1e62174be7a679c6513e13d8605f
MarcStorm <mslarsen1992@gmail.com>
2018-01-22 10:14:15 +0100

    Added config to space functions

--- gospace.go
+++ gospace.go
@@ -1,6 +1,8 @@
 package gospace
 
 import (
+	"crypto/tls"
+
 	shr "github.com/pspaces/gospace/shared"
 	spc "github.com/pspaces/gospace/space"
 )
@@ -10,13 +12,13 @@ type Tuple = shr.Tuple
 type Template = shr.Template
 
 // NewSpace creates a structure that represents a space.
-func NewSpace(name string) Space {
-	return spc.NewSpace(name)
+func NewSpace(name string, config *tls.Config) Space {
+	return spc.NewSpace(name, config)
 }
 
 // NewSpace creates a structure that represents a remote space.
-func NewRemoteSpace(name string) Space {
-	return spc.NewRemoteSpace(name)
+func NewRemoteSpace(name string, config *tls.Config) Space {
+	return spc.NewRemoteSpace(name, config)
 }
 
 // SpaceInterface contains all interfaces that can operate on a space.
